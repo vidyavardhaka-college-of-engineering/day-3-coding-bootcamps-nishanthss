4f9f4d36dd2881ff6429757be4159bee0f1e3d74
nishanthss <>
2021-05-26 11:11:21 +0000

    found the sum of n numbers

--- Problem_1.c
+++ Problem_1.c
@@ -9,15 +9,17 @@
 
 int main()
 {
-    int N;
+    int N,i,sum=0;
     printf("Enter the value for N\n");
     scanf("%d", &N);
 
     //Write your code here
+    for(i=1;i<=N;i++){
+      sum=sum+i;
+    }
+       
+    printf("The sum %d numbers is %d",N,sum);   
        
        
-       
-       
-       
-    return 0;
+     return 0;
 }
