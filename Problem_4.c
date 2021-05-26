490dcee736e91568e21705197d127347834ce663
nishanthss <>
2021-05-26 12:04:39 +0000

    pyramid of stars

--- Problem_4.c
+++ Problem_4.c
@@ -5,12 +5,17 @@
 
 int main()
 {
-  	int n;
-
+  	int n,i,j;
+    printf("Enter the value of\n");
   	scanf("%d", &n);
 
   	//Write your code here
-    
+    for(i=1;i<=n;i++){
+      for(j= 1;j<=i;j++){
+        printf("*");
+      }
+      printf("\n");
+    }
   	
 
   	return 0;
